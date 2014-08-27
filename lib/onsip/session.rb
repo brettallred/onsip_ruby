f2bee743e090031b978d020b40d5197ff8d6fa9c
Brett Allred <brettallred@gmail.com>
2014-08-27 16:04:05 -0600

    Fix Namespacing

--- lib/onsip/session.rb
+++ lib/onsip/session.rb
@@ -1,48 +1,49 @@
 require 'singleton'
+module Onsip
+  class Session < ::Onsip::RemoteResource
+    include ::Singleton
 
-class Onsip::Session < Onsip::RemoteResource
-  include ::Singleton
+    ##
+    # INSTANCE METHODS
+    #
+    def authenticate(username, password)
 
-  ##
-  # INSTANCE METHODS
-  #
-  def authenticate(username, password)
+      get({ :Action => "SessionCreate",
+            :Username => username,
+            :Password => password })
 
-    get({ :Action => "SessionCreate",
-          :Username => username,
-          :Password => password })
+      self.authenticated = true
 
-    self.authenticated = true
+      return authenticated? 
+    end
 
-    return authenticated? 
+    def authenticated?
+      return @authenticated
+    end
+
+    def destroy
+      get({ :Action => "SessionDestroy",
+            :SessionId => id })
+
+      self.authenticated = false
+
+      return last_response["Response"]["Context"]["Session"]["IsEstablished"] == "false"
+    end
+
+    def echo 
+      get({ :Action => "Echo" })
+      return last_response
+    end
+
+    def id 
+      @id ||= last_response["Response"]["Context"]["Session"]["SessionId"]
+    end
+
+    private 
+
+    def authenticated=(authenticated)
+      @authenticated = authenticated
+    end
+
   end
-
-  def authenticated?
-    return @authenticated
-  end
-
-  def destroy
-    get({ :Action => "SessionDestroy",
-          :SessionId => id })
-
-    self.authenticated = false
-
-    return last_response["Response"]["Context"]["Session"]["IsEstablished"] == "false"
-  end
-
-  def echo 
-    get({ :Action => "Echo" })
-    return last_response
-  end
-
-  def id 
-    @id ||= last_response["Response"]["Context"]["Session"]["SessionId"]
-  end
-
-  private 
-
-  def authenticated=(authenticated)
-    @authenticated = authenticated
-  end
-
 end
